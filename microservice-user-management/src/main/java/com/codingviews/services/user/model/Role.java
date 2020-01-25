ce297561ccf155b23572205acb22bd9a7e5a3217
Jimmy S. Emmanuel <jsemmanuel@gmail.com>
2020-01-25 15:19:47 -0500

    first commit

---
+++ microservice-user-management/src/main/java/com/codingviews/services/user/model/Role.java
@@ -0,0 +1,6 @@
+package com.codingviews.services.user.model;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ microservice-user-management/src/main/java/com/codingviews/services/user/model/User.java
@@ -0,0 +1,82 @@
+package com.codingviews.services.user.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(userName, user.userName) &&
+                Objects.equals(password, user.password) &&
+                role == user.role;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, userName, password, role);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", User.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .add("userName='" + userName + "'")
+                .add("password='" + password + "'")
+                .add("role=" + role)
+                .toString();
+    }
+}
