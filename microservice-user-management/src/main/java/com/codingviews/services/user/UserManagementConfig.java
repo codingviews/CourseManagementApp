6c2e461158b684bf407340d0fe7212e531280629
Jimmy S. Emmanuel <jsemmanuel@gmail.com>
2020-01-28 23:29:27 -0500

    Enabled Spring Security. Created new UserDetailService and Web Security Configurations.

--- microservice-user-management/src/main/java/com/codingviews/services/user/UserManagementConfig.java
+++ microservice-user-management/src/main/java/com/codingviews/services/user/UserManagementConfig.java
@@ -5,18 +5,11 @@ import com.codingviews.services.user.validator.UserValidator;
 import com.codingviews.services.user.validator.Validator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class UserManagementConfig {
     @Bean
     public Validator<User> userValidator() {
         return new UserValidator();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
---
+++ microservice-user-management/src/main/java/com/codingviews/services/user/service/UserDetailServiceImpl.java
@@ -0,0 +1,39 @@
+package com.codingviews.services.user.service;
+
+import com.codingviews.services.user.model.User;
+import com.codingviews.services.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUserName(username).orElse(null);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+
+        return new org.springframework.security.core.userdetails.User(username, user.getPassword(), Collections.unmodifiableSet(authorities));
+    }
+}
